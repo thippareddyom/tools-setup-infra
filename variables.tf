459de06dc6f7b97c50113c0f1d8308c637add620
kishor <kishorebsc12@gmail.com>
2026-02-10 17:23:04 +0530

    added

--- variables.tf
+++ variables.tf
@@ -4,12 +4,20 @@ variable "tools" {
     #   instance_type="t3.micro"
     #   port= 8200
     # }
-    github-runner={
-      instance_type= "t3.micro"
-      port= 443
+    # github-runner={
+    #   instance_type= "t3.micro"
+    #   port= 443
+    #   iam_policy= {
+    #     Action =   ["*"]
+    #     Resource = []
+    #   }
+    # }
+    jenkins= {
+      instance_type = "t3.micro"
+      port          = 8080
       iam_policy= {
-        Action =   ["*"]
-        Resource = []
+        Action  = []
+        Resource= []
       }
     }
   }
